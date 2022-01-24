0575536692d06827ff36ea4accdca1249fd9f1fd
wookshin <wookshins@gmail.com>
2022-01-25 08:35:12 +0900

    JS: active board에 todo 아이템 추가 기능

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
 'use strict'
-/* 텍스트 박스에 To-do를 입력 후 엔터를 쳤을 때 첫번째 Board에 반영된다 */
+
+/* 변수 */
 var registerInput = document.querySelector('.register__input');
+var registerBtn = document.querySelector('.register__btn');
+var boardsContainer = document.querySelector('.boards__container');
+var boardHeaders = document.querySelectorAll('.board__header');
+
+/* 텍스트 박스에 To-do를 입력 후 엔터를 쳤을 때 active Board Or 첫번째 Board에 반영된다 */
 registerInput.addEventListener('keydown', e => {
   if (e.key === 'Enter') {
-    addToDoAtFirstBoard();
+    addToDo();
   }
 })
 
-/* 텍스트 박스에 To-do를 입력 후 Add 버튼을 눌렀을 때 첫번째 Board에 반영된다 */
-var registerBtn = document.querySelector('.register__btn');
+/* 텍스트 박스에 To-do를 입력 후 Add 버튼을 눌렀을 때 active Board Or 첫번째 Board에 반영된다 */
 registerBtn.addEventListener('click', () => {
-  addToDoAtFirstBoard();
+  addToDo();
 })
 
 /* 휴지통 아이콘을 누르면 해당 아이템이 삭제된다 */
-var boardsContainer = document.querySelector('.boards__container');
 boardsContainer.addEventListener('click', e => {
   if (e.target.matches('.fa-trash-alt')) {
     var item = e.target.parentElement.parentElement;
     console.log(e.target);
     item.parentElement.removeChild(item);
+  }
+})
+
+/* Board Title을 클릭하면 해당 Board가 Active 상태가 된다 */
+/* 이미 Active 상태인 Board가 있다면 Active 상태를 없앤다 */
+boardsContainer.addEventListener('click', e => {
+  if (e.target.matches('.board__header')) {
+    boardHeaders.forEach(boardHeader => {
+      if (boardHeader === e.target) {
+        e.target.classList.toggle('active');
+      } else {
+        boardHeader.classList.remove('active')
+      }
+    });
   }
 })
 
@@ -35,13 +53,29 @@ function addToDoIntoBoard(board, todo) {
   board.children[1].appendChild(item);
   setTimeout(() => {
     item.classList.remove('create');
-  }, 100); 
-  
+  }, 100);   
 }
 
-function addToDoAtFirstBoard() {
+function addToDoBySelector(selector) {
   let todo = registerInput.value;
-  let firstBoard = document.querySelector('.board');
+  if (todo === '') return;
+  let firstBoard = document.querySelector(selector);
   addToDoIntoBoard(firstBoard, todo)
   registerInput.value = '';
+}
+
+function addToDoByElement(elem) {
+  let todo = registerInput.value;
+  addToDoIntoBoard(elem, todo)
+  registerInput.value = '';
+}
+
+function addToDo() {  
+  var activeBoard = document.querySelector('.board__header.active');
+
+  if (activeBoard == null) {
+    addToDoBySelector('.board');
+  } else {
+    addToDoByElement(activeBoard.parentElement);
+  }
 }
